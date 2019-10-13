465fbeb7386cd4be50aa034e2c721780b9cf11c4
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-10-13 17:52:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,15 +3,15 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  sort(array)
 end
 
 def using_reverse(array)
-
+  reverse(array)
 end
 
 def using_first(array)
-
+  first(array)
 end
 
 def using_last(array)
